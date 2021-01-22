8bbbcb1bbee1d0672c20ed515eb11405feb58512
ros-tel <vladvladow@yandex.ru>
2021-01-22 10:22:19 +0500

    Возможность добавить меню для перевода

--- src/bot/messages/text.go
+++ src/bot/messages/text.go
@@ -30,6 +30,8 @@ const (
 	MESSAGE_TREATMENT_START_BY_SPEC MessageType = 81
 	MESSAGE_TREATMENT_CLOSE         MessageType = 82
 	MESSAGE_TREATMENT_CLOSE_ACTIVE  MessageType = 90
+
+	MESSAGE_TREATMENT_TO_BOT MessageType = 200
 )
 
 type (
@@ -42,6 +44,9 @@ type (
 		MessageAuthor *uuid.UUID  `json:"author_id" binding:"omitempty" example:"4e48509f-6366-4897-9544-46f006e47074"`
 		MessageTime   string      `json:"message_time" binding:"required" example:"1"`
 		Text          string      `json:"text" example:"Привет"`
+		Data          struct {
+			Redirect string `json:"redirect"`
+		} `json:"data"`
 	}
 )
 
